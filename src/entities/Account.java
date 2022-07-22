00fcfa4027fc0f31a4f877aba46d2cf2a6da6565
Antonio Alan <tonio.alan07@gmail.com>
2022-07-22 16:43:16 -0300

    Account Holder

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -1,11 +1,30 @@
 package entities;
 
 public class Account {
-    private String holder;
     private Integer agency;
     private Integer number;
     private Double balance;
 
+    public void deposit(double value){
+        this.balance += value;
+    }
 
+    public boolean withdraw(double value){
+        if(this.balance >= value){
+            this.balance -= value;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public  boolean transfer(double value, Account account){
+        if(this.balance >value){
+            this.balance -= value;
+            account.deposit(value);
+            return true;
+        }
+        return false;
+    }
 
 }
