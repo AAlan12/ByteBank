39e8f804652c7d1dd3e460afcbff21444dca382a
Antonio Alan <tonio.alan07@gmail.com>
2022-07-24 22:06:05 -0300

    test on the account and creation of the bonus control

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,47 @@
 package application;
 
 import entities.Account;
+import entities.AccountHolder;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
-        Account account = new Account();
+        Locale.setDefault(Locale.US);
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = s.nextLine();
+        System.out.print("Cpf: ");
+        String cpf = s.nextLine();
+        System.out.print("Profession: ");
+        String profession = s.nextLine();
+        System.out.print("Agency: ");
+        Integer agency = s.nextInt();
+        System.out.print("Number: ");
+        Integer number = s.nextInt();
+        System.out.print("Balance: ");
+        double balance = s.nextDouble();
+
+        AccountHolder holder = new AccountHolder();
+        holder.setName(name);
+        holder.setCpf(cpf);
+        holder.setProfession(profession);
+
+        Account account = new Account(agency, number);
+        account.setAgency(agency);
+        account.setNumber(number);
+        account.setBalance(balance);
+        account.setHolder(holder);
+
+        System.out.println(account.getHolder().getName());
+        System.out.println(account.getAgency());
+        System.out.println(account.getNumber());
+        System.out.printf("R$ %.2f", account.getBalance());
+
 
     }
 }
--- src/entities/Account.java
+++ src/entities/Account.java
@@ -4,6 +4,57 @@ public class Account {
     private Integer agency;
     private Integer number;
     private Double balance;
+    private AccountHolder holder;
+    private static int total;
+
+    public Account(){
+
+    }
+
+    public  Account(Integer agency, Integer number){
+        Account.total ++;
+        System.out.println("Total accounts: " +Account.total);
+        System.out.println();
+        this.agency = agency;
+        this.number = number;
+        System.out.println("Account is being created " +this.number);
+    }
+
+    public Integer getAgency() {
+        return agency;
+    }
+
+    public void setAgency(Integer agency) {
+        this.agency = agency;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public Double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(Double balance) {
+        this.balance = balance;
+    }
+
+    public AccountHolder getHolder() {
+        return holder;
+    }
+
+    public void setHolder(AccountHolder holder) {
+        this.holder = holder;
+    }
+
+    public static int getTotal(){
+        return Account.total;
+    }
 
     public void deposit(double value){
         this.balance += value;
